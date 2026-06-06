2838ec29b7841aa3c77bf231b6358ab2aeb82e7c
arisu23 <dottedone23@gmail.com>
2026-06-06 21:52:23 +0800

    feat(auth): add auth interfaces - Created uth.interfaces.ts defining shapes for registration, login inputs, and user responses.

---
+++ backend/src/interfaces/auth.interfaces.ts
@@ -0,0 +1,34 @@
+import { UserRole } from '@prisma/client';
+
+export interface RegisterInput {
+  applicantId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  cellphone: string;
+  dateOfBirth: string;
+  program: string;
+  programType: string;
+  undergraduateCourse: string;
+  password: string;
+}
+
+export interface LoginInput {
+  role: string;
+  password: string;
+  email?: string;
+  applicantId?: string;
+  studentId?: string;
+  birthdate?: string;
+}
+
+export interface UserResponse {
+  id: string;
+  email: string;
+  role: UserRole;
+}
+
+export interface LoginResponse {
+  token: string;
+  user: UserResponse;
+}
---
+++ backend/src/repositories/auth.repository.ts
@@ -0,0 +1,53 @@
+import prisma from '../config/database';
+import { Prisma, User, Student, Program } from '@prisma/client';
+
+export class AuthRepository {
+    async findUserByEmail(email: string): Promise <User | null> {
+        return prisma.user.findUnique({
+            where: { email },
+        });
+    }
+
+    async findStudentByApplicantId(applicantId: string): Promise <(Student & { user: User}) | null> {
+        return prisma.student.findFirst({
+            where: { pinnacleApplicantId: applicantId },
+            include: { user: true}
+        });
+    }
+
+    async findStudentByStudentNumber(studentNumber: string, dob?: Date): Promise <(Student & { user: User}) | null> {
+         const whereClause: any = { studentNumber };
+         if (dob) {
+            whereClause.dateOfBirth = dob;
+         }
+
+         return prisma.student.findFirst({
+            where: whereClause,
+            include: { user: true }
+         });
+    }
+
+    async findProgramByName(programName: string): Promise <Program | null> {
+        return prisma.program.findFirst({
+            where: { programName }
+        });
+    }
+
+    //Create both User and Student atomically for a new applicant
+    async registerApplicant(userData: any, studentData: any): Promise <User> {
+        return prisma.$transaction(async (tx) => {
+            const newUser = await tx.user.create({
+                data: userData
+            });
+
+            await tx.student.create({
+                data: {
+                    ...studentData,
+                    userId: newUser.id,
+                }
+            });
+
+            return newUser;
+        });
+    }
+}
