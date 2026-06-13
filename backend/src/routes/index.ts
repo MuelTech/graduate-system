af9785b0df7d73bfbf4ab0b3cdb11e5a84826c83
arisu23 <dottedone23@gmail.com>
2026-06-13 21:23:57 +0800

    Plug Applicant route into index.ts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes';
 import programRoutes from './program.routes';
+import applicantRoutes from './applicant.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/programs', programRoutes);
+router.use('/applicant', applicantRoutes);
 
 export default router;
