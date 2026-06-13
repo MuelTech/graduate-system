5f578c849ca40c489884e07a6701b9b8b3552fcb
arisu23 <dottedone23@gmail.com>
2026-06-13 21:19:05 +0800

    feat(backend): add applicant routes

---
+++ backend/src/routes/applicant.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { ApplicantController } from "../controllers/applicant.controller";
+import { authenticateJWT, requireRole } from "../middlewares/auth.middleware";
+
+const router = Router();
+const applicantController = new ApplicantController();
+
+//GET api/applicant/profile
+//authenticatJWT checks if they are logged in
+//requireRole makes sure they are an AAPLICANT
+router.get(
+  "/profile",
+  authenticateJWT,
+  requireRole(["APPLICANT"]),
+  applicantController.getProfile,
+);
+
+export default router;
